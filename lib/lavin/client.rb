2b6077cb67f4f1335d70bebc3a452d325fea3cc3
Sammy Henningsson <sammy.henningsson@hemnet.se>
2022-10-26 15:29:54 +0200

    Support cookies

--- lib/lavin/client.rb
+++ lib/lavin/client.rb
@@ -17,7 +17,7 @@ module Lavin
     }.freeze
 
     attr_reader :internet, :base_url
-    attr_accessor :request_count
+    attr_accessor :request_count, :cookie
 
     def initialize(base_url = nil)
       raise NoCurrentAsyncTaskError unless Async::Task.current?
@@ -25,6 +25,7 @@ module Lavin
       @internet = Async::HTTP::Internet.new
       @base_url = base_url
       @request_count = 0
+      @cookie = nil
     end
 
     def close
@@ -32,23 +33,48 @@ module Lavin
     end
 
     def request(method, url:, headers:, body: nil)
+      url, headers, body = rewrite_request(url:, headers:, body:)
+
+      start_time = Time.now
+      response = internet.send(method, url, headers, body)
+      duration = Time.now - start_time
+
+      status, headers, body = process(response)
+
+      Statistics.register_request(method:, url:, status:, duration:)
+
+      {status:, headers:, body: body}
+    end
+
+    private
+
+    def rewrite_request(url:, headers:, body:)
       headers = DEFAULT_HEADERS.merge(headers || {})
+      headers["Cookie"] = cookie if cookie
+
       if body.is_a? Hash
         body = JSON.dump(body)
         headers["Content-Type"] = "application/json"
       end
 
       url = File.join(base_url, url) if base_url && !url.start_with?(/https?:/)
-      start_time = Time.now
-      response = internet.send(method, url, headers, body)
-      duration = Time.now - start_time
+
+      [url, headers, body]
+    end
+
+    def process(response)
       status = response.status
       headers = response.headers
       body = response.read
+      save_cookie(headers)
       self.request_count += 1
-      Statistics.register_request(method:, url:, status:, duration:)
 
-      {status:, headers:, body: body}
+      [status, headers, body]
+    end
+
+    def save_cookie(headers)
+      cookie = headers['Set-Cookie'] || headers['SET-COOKIE'] || headers['set-cookie']
+      self.cookie = cookie if cookie
     end
   end
 end
